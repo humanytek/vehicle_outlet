da5697ac9dee6a6b8e8aa6f37b07dcc1312233f1
AfroMonkey <moisalejandro@gmail.com>
2016-12-08 14:18:04 -0600

    [IMP] fun_transfer & _do_enter_transfer_details

--- models/vehicle_outlet.py
+++ models/vehicle_outlet.py
@@ -18,6 +18,8 @@ class VehicleOutlet(models.AbstractModel):
     product_id = fields.Many2one('product.product', compute="_compute_product_id", readonly=True, store=False)
     location_id = fields.Many2one('stock.location', readonly=True, related="contract_id.warehouse_id.wh_input_stock_loc_id")
 
+    exceeded = fields.Boolean(readonly=True)
+
     @api.one
     @api.depends('contract_id')
     def _compute_hired(self):
@@ -45,13 +47,14 @@ class VehicleOutlet(models.AbstractModel):
     @api.multi
     def fun_transfer(self):
         self.stock_picking_id = self.env['stock.picking'].search([('origin', '=', self.contract_id.name), ('state', '=', 'confirmed')], order='date', limit=1)
-        if not self.stock_picking_id:
+        if self.stock_picking_id:
+            self.stock_picking_id.force_assign()
+        else:
             self.stock_picking_id = self.env['stock.picking'].search([('origin', '=', self.contract_id.name), ('state', '=', 'assigned')], order='date', limit=1)
         if self.stock_picking_id:
-            self.stock_picking_id.action_assign()
-            if self.stock_picking_id.state == 'assigned':
-                picking = [self.stock_picking_id.id]
-                self._do_enter_transfer_details(picking, self.stock_picking_id, self.raw_kilos, self.location_id)
+            if self.raw_kilos > self.stock_picking_id.move_lines[0].product_uom_qty:
+                self.exceeded = True
+            self._do_enter_transfer_details()
 
     @api.multi
     def fun_ship(self):
@@ -60,37 +63,28 @@ class VehicleOutlet(models.AbstractModel):
             stock_picking_id_cancel.action_cancel()
 
     @api.multi
-    def _do_enter_transfer_details(self, picking_id, picking, raw_kilos, location_id, context=None):
-        if not context:
-            context = {}
-        else:
-            context = context.copy()
-        context.update({
-            'active_model': self._name,
-            'active_ids': picking_id,
-            'active_id': len(picking_id) and picking_id[0] or False
-        })
-
+    def _do_enter_transfer_details(self):
+        picking_id = [self.stock_picking_id.id]
         created_id = self.env['stock.transfer_details'].create({'picking_id': len(picking_id) and picking_id[0] or False})
-
         items = []
-        if not picking.pack_operation_ids:
-            picking.do_prepare_partial()
-        for op in picking.pack_operation_ids:
-            item = {
-                'packop_id': op.id,
-                'product_id': op.product_id.id,
-                'product_uom_id': op.product_uom_id.id,
-                'quantity': raw_kilos/1000,
-                'package_id': op.package_id.id,
-                'lot_id': op.lot_id.id,
-                'sourceloc_id': op.location_id.id,
-                'destinationloc_id': op.location_dest_id.id,
-                'result_package_id': op.result_package_id.id,
-                'date': op.date,
-                'owner_id': op.owner_id.id,
-            }
+        if not self.stock_picking_id.pack_operation_ids:
+            self.stock_picking_id.do_prepare_partial()
+        for op in self.stock_picking_id.pack_operation_ids:
             if op.product_id:
+                item = {
+                    'packop_id': op.id,
+                    'product_id': op.product_id.id,
+                    'product_uom_id': op.product_uom_id.id,
+                    'quantity': self.raw_kilos/1000,
+                    'package_id': op.package_id.id,
+                    'lot_id': op.lot_id.id,
+                    'sourceloc_id': op.location_id.id,
+                    'destinationloc_id': op.location_dest_id.id,
+                    'result_package_id': op.result_package_id.id,
+                    'date': op.date,
+                    'owner_id': op.owner_id.id,
+                }
                 items.append(item)
+                break
         created_id.item_ids = items
         created_id.do_detailed_transfer()
